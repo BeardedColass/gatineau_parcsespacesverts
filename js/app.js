278bbf9ae33dcd053c57dd39d97af2e3ead52d72
Nicolas Carriere <carriere.nic@gmail.com>
2012-04-01 18:19:12 -0400

    Ajout du auto complete, search bar et comptes des nombres de parcs par secteurs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 $(function () { //jQuery .ready handler
+    
+
+    
     App.init();
+      
 }); 
 
 App = (function () {
@@ -24,9 +28,14 @@ App = (function () {
         displayedMarkers = [],
         activeFilters = {
             sectors: {},
-            installations: {}
+            installations: {},
+            search: ""
         },
         cachedMarkers = null,
+        
+        sectorsCounts = {},
+        allSectors = {},
+        parcNom = [],
         searchInput, map, infoWindow, markerClusterer;
     
     /**
@@ -39,31 +48,42 @@ App = (function () {
         
         var filteredMarkers = [],
             sectorsFilter = filters.sectors,
-            installationsFilter = filters.installations;
+            installationsFilter = filters.installations,
+            searchFilter = filters.search,
+            searchRX = new RegExp(searchFilter, 'i');
+            
             
         $.each(markers, function (i, marker) {
             var hasAtLeastOneSector = false;
             var accept = false;
+            var reject = true;
             
-            $.each(sectorsFilter, function (sector) {
-                hasAtLeastOneSector = true;
-                if (marker.sector === sector) {
-                    accept = true;
-                    return false; // break the foreach loop
-                }
-            });
-        
-            // reject by sector: at least on sector is checked, and NO selected sectors match
-            var reject = hasAtLeastOneSector && !accept;
-        
-            // reject by installation: reject if any selected installation is not present
-            $.each(installationsFilter, function (installation) {
-                if (!marker.hasInstallation[installation]) {
-                    reject = true;
-                    return false; // break the foreach loop
-                }
-            });
-        
+            
+                
+            if (searchFilter){
+                    if (searchRX.test(marker.name)){
+                        reject= false;
+                    }
+            } else{
+                $.each(sectorsFilter, function (sector) {
+                    hasAtLeastOneSector = true;
+                    if (marker.sector === sector) {
+                        accept = true;
+                        return false; // break the foreach loop
+                    }
+                });
+            
+                // reject by sector: at least on sector is checked, and NO selected sectors match
+                reject = hasAtLeastOneSector && !accept;
+            
+                // reject by installation: reject if any selected installation is not present
+                $.each(installationsFilter, function (installation) {
+                    if (!marker.hasInstallation[installation]) {
+                        reject = true;
+                        return false; // break the foreach loop
+                    }
+                });
+            }
             if (!reject) {
                 filteredMarkers.push(marker);
             }
@@ -73,7 +93,6 @@ App = (function () {
     
     function markersPostProcessing(markers) {
         
-        var allSectors = {};
         var allInstallations = {};
         
         $.each(markers, function (i, marker) {
@@ -91,8 +110,13 @@ App = (function () {
         var $sectors = $('#sectors');
         
         $.each(sortedSectors, function (i, sector) {
-            $sectors.append($('<input type="checkbox" name="sectors" value="' + sector + '"/> <span>' + sector + '</span>'));
+            $sectors.append($('<input type="checkbox" name="sectors" value="' + sector + '"/> <span>' + sector + '</span> <span id="' + sector +  '_count"></span>'));
         });
+        
+        // initialize sectors counts
+        for (var k in allSectors) {
+            sectorsCounts[k] = 0;
+        }
         
         // sort and insert installation checkboxes
         var sortedInstallations = c7nSortedHashKeys(allInstallations);
@@ -109,6 +133,8 @@ App = (function () {
         });
         
         $('#installations table').append($trows);
+        
+        
     }
     
     /**
@@ -153,6 +179,8 @@ App = (function () {
     function addMarkersToMap(markers) {
         var mapMarker, marker, icon;
         
+        
+        
         for (var i = 0, len = markers.length; i < len; i++) {
             
             mapMarker = new GMarker({
@@ -169,9 +197,13 @@ App = (function () {
             mapMarker._data = marker;
             
             GEvent.addListener(mapMarker, 'click', onMarkerClick);
+            
+            $(document).trigger('markerAdded', [marker]);
         }
         
         markerClusterer.addMarkers(displayedMarkers);
+        
+        $(document).trigger('allMarkersAdded', [displayedMarkers]);
     }
     
     function removeAllMarkersFromMap() {
@@ -189,6 +221,8 @@ App = (function () {
     }
     
     function refreshMarkers() {
+        
+        $(document).trigger('beforeRefreshMarkers');
         removeAllMarkersFromMap();
         getMarkers(activeFilters, addMarkersToMap);
     }
@@ -206,14 +240,75 @@ App = (function () {
         $(document).on('click', '[type=checkbox]', function (e) {
             var chk = e.target;
             
+            
             updateFilter(chk.name, chk.value, !chk.checked);            
             refreshMarkers();
+        });
+        
+        $(searchbar).bind("keypress", function(e) {
+            if (e.keyCode == 13) {
+                var search = document.getElementById('searchInput');
+                clearAllFilters();
+                activeFilters.search = search.value;
+                
+                refreshMarkers();         
+            }
         });
         
         GEvent.addListener(infoWindow, 'closeclick', function () {
             focusedMarker.setAnimation(null);
             focusedMarker = null;
         });
+        
+        $(document).on('markerAdded', function (e, marker) {
+            sectorsCounts[marker.sector] += 1;
+        });
+        
+        $(document).on('allMarkersAdded', function (){
+            updateCounts();
+        });
+        
+        $(document).on('beforeRefreshMarkers', function (){
+            resetCounts();
+        });
+        
+        $(document).one('allMarkersAdded', function(){
+            for (var i in  cachedMarkers){
+                parcNom[i] = cachedMarkers[i].name;
+            }
+            $("input#searchInput").autocomplete({
+            source: parcNom
+            }); 
+        });
+        
+        
+    }
+    
+    function updateCounts(){
+        for (var k in allSectors) {
+            document.getElementById(k + '_count').innerText = '(' + sectorsCounts[k] + ')';
+        }
+    }
+    
+    function clearAllFilters() {
+        var filters =  document.querySelectorAll('[type=checkbox]');
+        for (var i = 0 ; i < filters.length; i++) {
+            filters[i].checked = false;
+        }
+        
+        activeFilters = {
+            sectors: {},
+            installations: {},
+            search: ""
+        };
+    }
+
+    
+    function resetCounts(){
+        for (var k in sectorsCounts){
+            sectorsCounts[k] = 0; 
+        }
+        
         
     }
         
@@ -235,6 +330,8 @@ App = (function () {
             installListeners();
             
             refreshMarkers();
+            
+            
         }
     };
 })();
---
+++ php/phpsqlajax_dbinfo.php
@@ -0,0 +1,5 @@
+<?php
+$username="root";
+$password="";
+$database="gatineau_parcs_espaces_verts";
+?>
